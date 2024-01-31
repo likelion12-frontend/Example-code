0137edf02b36ecf9a7e258d3b84c42218bdc0fc9
Sengoo <lesley023@naver.com>
2024-01-31 15:27:13 +0900

    react router 예제 수정중

---
+++ week5/router/src/components/About.jsx
@@ -0,0 +1,10 @@
+function About(){
+    return(
+        <>
+        <h1>About</h1>
+        <div>여기는 About 입니다.</div>
+        </>
+    );
+}
+
+export default About;
---
+++ week5/router/src/components/Users.jsx
@@ -0,0 +1,10 @@
+function Users(){
+    return(
+        <>
+        <h1>Users</h1>
+        여기는 Users 입니다.
+        </>
+    );
+}
+
+export default Users;
