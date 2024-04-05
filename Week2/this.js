c4221158e5b9ca9acf3ceea15c7eb02d759574fb
Sengoo <lesley023@naver.com>
2024-04-05 16:31:10 +0900

    docs, css : 실습코드추가

--- Week2/this.js
+++ Week2/this.js
@@ -9,5 +9,16 @@ const user = {
     },
   };
 
-console.log(user.display1()); // name:user01 age:20
-console.log(user.display2()); // name:undefined age:undefined
+console.log(user.display1()); 
+console.log(user.display2()); 
+// name:user01 age:20
+// name:undefined age:undefined
+
+// const a=1;
+// console.log(a);
+
+// a=2;
+// console.log(a);
+
+// let a=3;
+// console.log(a);
