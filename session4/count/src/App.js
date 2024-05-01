2ac26afe2119430248eef6fd96126ed18cebd5ed
Jun279 <33.beautifulboy@gmail.com>
2024-05-01 18:24:56 +0900

    session4 useEffect 예제 코드 수정

--- session4/count/src/App.js
+++ session4/count/src/App.js
@@ -1,14 +1,17 @@
 import "./App.css";
 import { useEffect, useState } from "react";
 import "bootstrap/dist/css/bootstrap.min.css"; // npm install react-bootstrap bootstrap
+import Page from "./component/page";
+import { Route, Routes, useNavigate } from "react-router-dom";
 
-function App() {
+function Main() {
   const [goodCount, setGoodCount] = useState(0);
   const [notice, setNotice] = useState(false);
+  const navigate = useNavigate();
 
   const showNotice = () => {
     setNotice(true);
-  }
+  };
 
   const increaseGoodCount = () => {
     setGoodCount(goodCount + 1);
@@ -19,20 +22,19 @@ function App() {
   }, [goodCount]);
 
   useEffect(() => {
-    let a = setTimeout(() => {
+    let timer = 
+    setTimeout(() => {
       setNotice(false);
       console.log("폭탄 터짐 ㅅㄱ");
     }, 5000);
-    return () => {
-      clearTimeout(a);
-    }
+    return () => clearTimeout(timer);
   }, [notice]);
 
   return (
     <div className="App">
       {notice && (
         <div className="alert alert-warning">
-          <p>5초뒤 폭탄이 터집니다.</p>
+          <p>5초 후 폭탄이 터집니다.</p>
         </div>
       )}
       <h1>useEffect 사용법</h1>
@@ -42,7 +44,17 @@ function App() {
       <div className="timer">
         <button onClick={showNotice}>폭탄 실행.</button>
       </div>
+      <button onClick={() => navigate("/Page")}>이동</button>
     </div>
+  );
+}
+
+function App() {
+  return (
+    <Routes>
+      <Route path="/" element={<Main />} />
+      <Route path="Page" element={<Page />} />
+    </Routes>
   );
 }
 
